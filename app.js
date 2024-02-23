ab9853efb84b59e9850903094dc02150bfbb1955
Imran Remtulla <imranr98@pm.me>
2024-02-23 18:53:50 -0500

    Fix previous broken commit

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 import fs from 'fs'
 import * as pdfjs from 'pdfjs-dist'
 
